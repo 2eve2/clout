d0cb2ee9e82d2b9aaf909e9aadc29b0be51a1e44
DJ-archive <ydajeong7@gmail.com>
2023-12-04 16:32:59 +0900

    Chore: 포인트 서비스 통일화
    - 인프라 담당 김민식 팀원의 실수로 폴더 및 소스 코드가 유실되어 복구 작업

--- backend/point-service/src/main/java/com/mmm/clout/pointservice/PointServiceApplication.java
+++ backend/point-service/src/main/java/com/mmm/clout/pointservice/PointServiceApplication.java
@@ -1,10 +1,18 @@
 package com.mmm.clout.pointservice;
 
 import com.mmm.clout.pointservice.common.config.EnvConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.Date;
+import java.util.TimeZone;
+
+@Slf4j
+@EnableJpaAuditing
 @SpringBootApplication
 @PropertySource(value = {
 		"classpath:secret/env.yml",
@@ -15,4 +23,9 @@ public class PointServiceApplication {
 		SpringApplication.run(PointServiceApplication.class, args);
 	}
 
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		log.info("현재 시각: {}", new Date());
+	}
 }
